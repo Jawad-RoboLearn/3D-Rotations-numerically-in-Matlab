3da301233ef19165916e889c48874caad4aec3cb
Jawad Mehmood Butt <70571986+Jawad-RoboLearn@users.noreply.github.com>
2021-06-24 12:34:53 +0800

    Add files via upload

---
+++ euler2quaternion.m
@@ -0,0 +1,13 @@
+function [qw,qx,qy,qz] = euler2quaternion(euler)
+
+roll = euler(1); pitch = euler(2); yaw = euler(3);
+
+qw = cos(roll/2)*cos(pitch/2)*cos(yaw/2) + sin(roll/2)*sin(pitch/2)*sin(yaw/2);
+
+qx = sin(roll/2)*cos(pitch/2)*cos(yaw/2) - cos(roll/2)*sin(pitch/2)*sin(yaw/2);
+
+qy = cos(roll/2)*sin(pitch/2)*cos(yaw/2) + sin(roll/2)*cos(pitch/2)*sin(yaw/2);
+
+qz = cos(roll/2)*cos(pitch/2)*sin(yaw/2) - sin(roll/2)*sin(pitch/2)*cos(yaw/2);
+
+end
